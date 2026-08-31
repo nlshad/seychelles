75773d4fbeb0fc8855bab7dbdc990c5afa5b89c4
nlshad <nnishadt@gmail.com>
2026-08-31 17:25:27 +0530

    Overhaul mobile responsive architecture with 1-line top bar & compact header v21.0

--- includes/header.php
+++ includes/header.php
@@ -35,7 +35,7 @@ $page_keywords = $page_keywords ?? 'Door to Door delivery to india, Packing and 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <!-- Modern Main CSS Design System -->
-  <link rel="stylesheet" href="css/main.css?v=20.0">
+  <link rel="stylesheet" href="css/main.css?v=21.0">
 </head>
 <body>
 
